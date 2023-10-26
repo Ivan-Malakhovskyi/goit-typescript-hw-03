684d052152b225ba595c49b9c6398d3e333ad546
Ivan-Malakhovskyi <124193519+Ivan-Malakhovskyi@users.noreply.github.com>
2023-10-26 14:15:33 +0300

    completed

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,63 @@
+class Key {
+  private signature: number;
+
+  constructor() {
+    this.signature = Math.random();
+  }
+
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  private key: Key;
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  getKey() {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door: boolean = false;
+  public key: Key;
+  public tenants: Person[] = [];
+
+  comeIn(person: Person) {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log(`${person.getKey().getSignature()} enter in House`);
+    }
+    console.log("Door is close");
+  }
+
+  abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  constructor(key: Key) {
+    super();
+    this.key = key;
+  }
+
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+      console.log("Door is open");
+    }
+    console.log("Door is closed");
+  }
+}
+
 const key = new Key();
-
-const house = new MyHouse(key);
 const person = new Person(key);
-
+const house = new MyHouse(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
